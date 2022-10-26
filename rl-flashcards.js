09f5c4087cf08b8a42c39b2d742f6075b401a77e
Kaj Schmidt <kaj.schmidt@ntig.se>
2022-10-26 13:41:03 +0200

    Bygger modals

--- rl-flashcards.js
+++ rl-flashcards.js
@@ -28,24 +28,59 @@ class modelJSON {
 class viewCardline {
     constructor(controller) {
         this.controller = controller;
+        this.site= {};
+        this.site.body = document.querySelector("body");
+        this.site.container = document.querySelector(".rl_flashcards");
     }
 
     loadDeck(deck) {
-        console.log("deck");
+        //console.log("deck");
         for (let [index, section] of deck.entries()) {
             this.loadSection(section, index)
         }
     }
 
     loadSection(section, section_index) {
-        console.log("s"+ section_index);
+        //console.log("s"+ section_index);
         for (let [index, question] of section.questions.entries()) {
             this.loadQuestion(question, index, section_index)
         }
     } 
 
     loadQuestion(question, question_index, section_index) {
-        console.log("q"+ question_index);
+        let modal_id = "s" + section_index + "q" + question_index;
+
+        let modal = document.createElement("div");
+        modal.classList.add("modal");
+        modal.setAttribute("id", modal_id);
+
+        let modal_dialog = document.createElement("div");
+        modal_dialog.classList.add("modal-dialog");
+        
+
+        let modal_content = document.createElement("div");
+        modal_content.classList.add("modal-content");
+        
+        
+        let modal_header = document.createElement("div");
+        modal_header.classList.add("modal-header");
+        
+        let modal_body= document.createElement("div");
+        modal_body.classList.add("modal-body");
+        
+        let modal_footer = document.createElement("div");
+        modal_footer.classList.add("modal-footer");
+
+        modal_content.append(modal_header);
+        modal_content.append(modal_body);
+        modal_content.append(modal_footer);
+        modal_dialog.append(modal_content);
+        modal.append(modal_dialog);
+
+        this.site.body.append(modal);
+        
+
+        return modal_id;
 
     }
 }
