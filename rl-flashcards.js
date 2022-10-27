227ca0810092266877cb5668b140dac7704af57a
Kaj Schmidt <kaj.schmidt@ntig.se>
2022-10-27 09:02:08 +0200

    Settings

--- rl-flashcards.js
+++ rl-flashcards.js
@@ -5,7 +5,9 @@ class RLFlashcards {
     }
 
     init() {
-        this.view.loadDeck(this.data.store.deck);
+        this.data.loadData();
+        this.view.buildSite(this.data.store.deck, this.data.store.settings);
+        this.view.openGreeting();
     }
 }
 
@@ -13,10 +15,18 @@ class modelJSON {
     constructor(controller) {
         this.controller = controller;
         this.store = {
-            "settings":{},
+            "settings":{
+                "title":"Titel",
+                "greeting":"Välkommen",
+                "restart_at":"section"
+            },
             "user": {},
             "deck": []
         }
+    }
+
+    loadData() {
+
     }
 
     addSection(section) {
@@ -31,23 +41,44 @@ class viewCardline {
         this.site= {};
         this.site.body = document.querySelector("body");
         this.site.container = document.querySelector(".rl_flashcards");
+        this.site.modals = {};
     }
 
-    loadDeck(deck) {
+
+    buildSite(deck, settings) {
+        this.buildGreetings(settings);
+        this.buildFail(settings);
+        this.buildDone(settings);
+        this.buildDeck(deck, settings);
+    }
+
+    buildGreeting() {
+
+    }
+
+    buildFail() {
+
+    }
+
+    buildDone() {
+
+    }
+
+    buildDeck(deck, settings) {
         //console.log("deck");
         for (let [index, section] of deck.entries()) {
-            this.loadSection(section, index)
+            this.buildSection(section, index)
         }
     }
 
-    loadSection(section, section_index) {
+    buildSection(section, section_index, settings) {
         //console.log("s"+ section_index);
         for (let [index, question] of section.questions.entries()) {
-            this.loadQuestion(question, index, section_index)
+            this.buildQuestion(question, index, section_index)
         }
     } 
 
-    loadQuestion(question, question_index, section_index) {
+    buildQuestion(question, question_index, section_index, settings) {
         let modal_id = "s" + section_index + "q" + question_index;
 
         let modal = document.createElement("div");
@@ -69,40 +100,58 @@ class viewCardline {
         modal_body.classList.add("modal-body");
 
         let modal_buttons= document.createElement("div");
-        modal_buttons.classList.add("modal-body","list-group");
+        modal_buttons.classList.add("list-group", "list-group-flush");
 
         for (let answer of question.correct_answer) {
             let button = document.createElement("button");
-            button.classList.add("btn", "btn-primary", "list-group-item");
+            button.classList.add("list-group-item");
             button.innerHTML = answer;
             modal_buttons.append(button);
         }
 
         for (let answer of question.wrong_answer) {
             let button = document.createElement("button");
-            button.classList.add("btn", "btn-primary");
+            button.classList.add("list-group-item");
             button.innerHTML = answer;
-            modal_body.append(button);
+            modal_buttons.append(button);
         }
+
+
         
         let modal_footer = document.createElement("div");
         modal_footer.classList.add("modal-footer");
 
         modal_content.append(modal_header);
         modal_content.append(modal_body);
+        modal_content.append(modal_buttons);
         modal_content.append(modal_footer);
         modal_dialog.append(modal_content);
         modal.append(modal_dialog);
 
         this.site.body.append(modal);
-
+        this.site.modals[modal_id] = new bootstrap.Modal("#"+modal_id);
 
         return modal_id;
 
     }
 
-    openQuestion(id) {
-        let modal = new bootstrap.Modal("#"+id);
-        modal.show();
+    openGreeting() {
+
+    }
+
+    openSection() {
+
+    }
+
+    openQuestion(modal_id) {
+        this.site.modals[modal_id].show();
+    }
+
+    openFail() {
+
+    }
+
+    openDone() {
+
     }
 }
